14abc3cafc79406a94ae7f129f19c15a16331d1d
Alessio Leodori <alessio.leodori@gmail.com>
2023-09-06 11:07:24 +0200

    added if else

--- src/Base/If_else.java
+++ src/Base/If_else.java
@@ -1,7 +1,47 @@
 package Base;
 
+import java.util.Scanner;
+
 public class If_else {
     public static void main(String[] args) {
+        int n1 = 4, n2 = 5;
+        System.out.println(n1 > n2);
+        if(n1 < n2) {
+            System.out.println("This condition is true!");
+        }
+
+        // IF ELSE
+        if (n1 > n2) {
+            System.out.println("Condition is true");
+        } else {
+            System.out.println("Condition is false");
+        }
+
+        /* Exercise: ask a number to the user, if the number is higher than 10, log "Number is higher than 10", else
+        log "Number is lower than 10".
+         */
+    /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please insert a number: ");
+        int num = in.nextInt();
+
+        if (num > 10) {
+            System.out.println("Number is higher than 10");
+        } else {
+            System.out.println("Number is lower than 10");
+        }
+    */
+        // IF, ELSE IF, ELSE
+
+        int num1 = 6;
+
+        if (num1 > 10) {
+            System.out.println("Number is higher than 10");
+        } else if (num1 == 10) {
+            System.out.println("Number is equal to 10");
+        } else {
+            System.out.println("Number is lower than 10");
+        }
 
     }
 }
