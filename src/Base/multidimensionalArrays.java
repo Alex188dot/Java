f0952ad33f26529f182f9ea750ca24d1e0bdb9fa
Alessio Leodori <alessio.leodori@gmail.com>
2023-09-11 16:43:38 +0200

    added multidimensionalArrays

---
+++ src/Base/multidimensionalArrays.java
@@ -0,0 +1,18 @@
+package Base;
+
+public class multidimensionalArrays {
+    public static void main(String[] args) {
+        // Multidimensional arrays
+        String[][] classes = {
+                {"John", "Jane"},
+                {"Alex", "Harry"},
+        };
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.println("I: " + i + ", J: " + j);
+                System.out.println(classes[i][j]);
+            }
+        }
+    }
+}
