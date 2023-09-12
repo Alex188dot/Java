c96ddb8b506e837a727ecc81a15f2bb76b9320cb
Alessio Leodori <alessio.leodori@gmail.com>
2023-09-12 17:56:29 +0200

    Added arraylist exercise

--- src/Base/ArrayLists.java
+++ src/Base/ArrayLists.java
@@ -1,7 +1,9 @@
 package Base;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Scanner;
 
 public class ArrayLists {
     public static void main(String[] args) {
@@ -105,6 +107,66 @@ public class ArrayLists {
         System.out.println("Cleared ArrayList: " + persons);
 
         System.out.println("----------------------------");
+
+
+        // Exercise: create a To Do list. When you run the application, show all the to do things.
+        // If there is nothing to show, print "You have no items in your To Do List".
+
+
+        Scanner in = new Scanner(System.in);
+        ArrayList<String> thingsToDo = new ArrayList<>();
+        thingsToDo.add("Study Java");
+        thingsToDo.add("Buy groceries");
+        thingsToDo.add("Check emails");
+        thingsToDo.add("Do laundry");
+        System.out.println("These are the things in your To Do List: ");
+        int count = 0;
+        for (String el: thingsToDo) {
+            count++;
+            System.out.println(count + ") " + el);
+        }
+        System.out.println("----------------------------");
+
+        int choice;
+
+        do  {
+           System.out.println("Please select one of the options below:\n1) Add a To Do item;\n2) Delete a To Do item;\n3) Exit ");
+           choice = in.nextInt();
+           String item = in.nextLine();
+            if (choice == 1) {
+               System.out.println("Please type the item you want to add to your To Do List and then press Enter");
+               item = in.nextLine();
+               thingsToDo.add(item);
+               if (thingsToDo.size() > 0) {
+                   System.out.println("These are the things in your To Do List: ");
+                   count = 0;
+                   for (String el: thingsToDo) {
+                       count++;
+                       System.out.println(count + ") " + el);
+                   }
+               } else {
+                   System.out.println("You have no items in your To Do List");
+               }
+                System.out.println("----------------------------");
+            } else if (choice == 2) {
+            System.out.println("Please type the number of the item you want to remove: ");
+            int itemnum = in.nextInt();
+            thingsToDo.remove(itemnum - 1);
+            if (thingsToDo.size() > 0) {
+                System.out.println("These are the things in your To Do List: ");
+                count = 0;
+                for (String el: thingsToDo) {
+                    count++;
+                    System.out.println(count + ") " + el);
+                }
+            } else {
+                System.out.println("You have no items in your To Do List");
+            }
+            }
+        } while (choice != 3);
+
+
+
 
 
 
