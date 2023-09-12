b618ae3ed14f866662b92078a3f9d3ce59221cdf
Alessio Leodori <alessio.leodori@gmail.com>
2023-09-12 13:00:04 +0200

    Added WrapperClass

---
+++ src/Base/WrapperClass.java
@@ -0,0 +1,18 @@
+package Base;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+        // Reference types are heavier for the processor, so use them wisely!
+
+        String word = "Hello";
+        Integer numb = 30;
+        Character char1 = 'c';
+        Double doub = 14.4;
+        Boolean bool = false;
+
+        // Why use references? Because ArrayLists (an evolution of arrays) accept ONLY reference types.
+
+
+
+    }
+}
