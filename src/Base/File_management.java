bddeb5daf9b55c5f2ab59ed28dd797b11e220d22
Alessio Leodori <alessio.leodori@gmail.com>
2023-09-14 11:36:51 +0200

    added file management

--- src/Base/File_management.java
+++ src/Base/File_management.java
@@ -1,7 +1,12 @@
 package Base;
+import java.io.File;
 
 public class File_management {
     public static void main(String[] args) {
+
+        File file = new File("File/test.txt"); // This is to open the file
+        System.out.println(file.getPath());
+        System.out.println(file.getAbsolutePath());
 
 
 
