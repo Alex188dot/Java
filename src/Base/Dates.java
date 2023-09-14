1746bbc282df6016e5dcc959e36ac169da312c34
Alessio Leodori <alessio.leodori@gmail.com>
2023-09-14 08:53:39 +0200

    added date exercise

--- src/Base/Dates.java
+++ src/Base/Dates.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Scanner;
 
 
 public class Dates {
@@ -51,5 +52,55 @@ public class Dates {
         // Format full date
         DateTimeFormatter itCompleteDate = DateTimeFormatter.ofPattern("EEEE, dd MMMM yyyy HH:mm");
         System.out.println("Complete formatted date: " +completeDate.format(itCompleteDate));
+
+
+        System.out.println("------------------------");
+        /* Exercise: write a function that asks day, month and year and compares it with the current date. If it is the same
+         It will say "current date", if it is older it will say "older that", if it is newer, if will say "in the future".
+         Check if the day is between 1 and 31, the month is between 1 and 12.
+         Ternary operator:
+
+         */
+        System.out.println("Please insert a date and I will compare it: ");
+        System.out.println("Please insert the day: ");
+        Scanner in = new Scanner(System.in);
+        int day = in.nextInt();
+        day = day > 0 && day < 32 ? day : 1;
+        System.out.println("Please insert the month: ");
+        int month = in.nextInt();
+        month = month > 0 && month < 13 ? month : 1;
+        System.out.println("Please insert the year: ");
+        int year = in.nextInt();
+        year = year > 0 && year < 2024 ? year : 1;
+
+        String newDate = String.format("%02d", day) + "/" + String.format("%02d", month) + "/" + String.format("%02d", year);
+        System.out.println("Your date: " + newDate);
+
+        System.out.println("Today's date: " + date.format(itDateFormat));
+
+        System.out.println(date.format(itDateFormat).compareTo(newDate));
+
+        if (date.format(itDateFormat).compareTo(newDate) == 0) {
+            System.out.println("Your date is the current date!");
+        } else if (date.format(itDateFormat).compareTo(newDate) == -1) {
+            System.out.println("Your date is older than the current date");
+        } else if (date.format(itDateFormat).compareTo(newDate) == 1) {
+            System.out.println("Your date is in the future");
+        };
+
+        /*
+        if (userInp > 0 && userInp < 13) {
+            month = userInp;
+        } else {
+            month = 1;
+        }
+        */
+
+
+
+        // With the ternary operator
+
+
+
     }
 }
