db1d44b74c188130d1e4b1d0053abaa1880c5a8a
Alessio Leodori <alessio.leodori@gmail.com>
2023-09-18 15:11:08 +0200

    minor changes

--- src/Inheritance_Exercise/Product_start.java
+++ src/Inheritance_Exercise/Product_start.java
@@ -3,8 +3,6 @@ package Inheritance_Exercise;
 public class Product_start {
 
     public static void main(String[] args) {
-        Product p1 = new Product("PlayStation", "PlayStation 5 Console", 499);
-        System.out.println(p1);
         FoodProduct f1 = new FoodProduct("Cheese", "Some delicious cheese", 5, "19/09/2023");
 
         System.out.println(f1);
