a1704d1c497b6e5a63f1c6a97aca357d6a8d83b3
Alessio Leodori <alessio.leodori@gmail.com>
2023-09-19 15:04:29 +0200

    minor changes

--- src/DBManagement/MyDBConnection.java
+++ src/DBManagement/MyDBConnection.java
@@ -2,8 +2,13 @@ package DBManagement;
 import java.sql.*; //Library for DB use
 public class MyDBConnection {
     public static void main(String[] args) throws SQLException {
-
-        Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/DBManagement", "root", "root");
+        try {
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:8889/javaDB", "root", "root");
+            System.out.println("Connection successful");
+        } catch (SQLException e) {
+            System.out.println("Connection failed");
+            e.printStackTrace();
+        }
 
     }
 }
