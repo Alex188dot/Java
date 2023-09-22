b2a5c1ef1524f6c372b9e69200895f197bec4224
Alessio Leodori <alessio.leodori@gmail.com>
2023-09-22 16:34:20 +0200

    moved files to relevant folder

---
+++ PHP/examples/arrays.php
@@ -0,0 +1,32 @@
+<a href="https://www.google.com">Google</a>
+<?php
+
+$name = "John"; // this is a string variable
+$lastname = "Doe";
+$eta = 30; // Integer variable
+
+$ar = ["John", "Michael", "Jane", "Mary"];
+$ar_mixed = ["John", 44, true, "Mary", [1, "PHP", 3]];
+
+//echo $ar_mixed[4][1];
+echo $ar_mixed[0]."<br>";
+
+// to concatenate use the . 
+
+echo $name."<br>";
+echo $name."<br>";
+
+echo $name."<br>";
+
+
+// To get the lenght of an array
+echo count($ar);
+
+// Push method
+
+
+
+// How to visualize the whole array
+print_r($ar);
+
+?>
---
+++ PHP/examples/receive.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Name: " . $_POST["name"] . "<br>";
+echo "Lastname: " . $_POST["lastname"] . "<br>";
+echo "Password: " . $_POST["password"] . "<br>";
+
+?>
