52b541da573e5dead3886a700e66ba0317022acc
Alessio Leodori <alessio.leodori@gmail.com>
2023-09-25 14:13:37 +0200

    add user and view users features

--- PHP/layout/template/base.php
+++ PHP/layout/template/base.php
@@ -12,15 +12,15 @@
     </style>
 </head>
 <body>
-<?php include("template/navbar.php"); ?>
+<?php include("navbar.php"); ?>
 <!-- body -->
 <div class="container">
-    <?php include("template/selectionFromDB.php"); ?>
-
+    <?php include("selectionFromDB.php"); ?>
+        
 
 </div>
 <!-- end body  -->
-<?php include("template/footer.php"); ?>
+<?php include("footer.php"); ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
