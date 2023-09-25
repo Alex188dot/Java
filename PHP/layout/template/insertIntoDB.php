740ad41900c4cc02fa580aae19bf0c694e93455f
Alessio Leodori <alessio.leodori@gmail.com>
2023-09-25 11:05:00 +0200

    minor changes

--- PHP/layout/template/insertIntoDB.php
+++ PHP/layout/template/insertIntoDB.php
@@ -10,19 +10,19 @@ if ($conn->connect_error) {
 
 $name = "John";
 $lastname = "Doe";
-$age = 30;
 $city = "New York";
+$age = 30;
 $office_id = 1;
 
 $query = "INSERT INTO users VALUES(NULL, '$name', '$lastname', '$city', $age, $office_id);";
 
 
-$result = $conn->query($query); // Call the query method once and store the result
+$result = $conn->query($query);
 
-if ($result === true) { // Check the result and handle success
+if ($result === true) { 
     echo "New record created successfully";
     echo "The id is: " . $conn->insert_id;
-} else { // Handle error
+} else { 
     echo "Error: " . $query . "<br>" . $conn->error;
 }
 ?>
