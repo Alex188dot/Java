27e802d8205459ee17c134d2df8267d3072cf90c
Alessio Leodori <alessio.leodori@gmail.com>
2023-09-25 14:13:50 +0200

    added footer

--- PHP/layout/template/footer.php
+++ PHP/layout/template/footer.php
@@ -1,5 +1,5 @@
 <!-- footer -->
-<footer class="py-5 text-center text-body-secondary bg-body-tertiary">
+<footer class="text-center text-body-secondary bg-body-tertiary">
   <p>Copyright © 2023</p>
 </footer>
 <!-- end footer  -->
